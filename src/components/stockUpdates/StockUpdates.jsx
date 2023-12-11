3ee57044a028f9ef1088d9b934edac0788207131
Binud Panging <95073560+devBinud@users.noreply.github.com>
2023-12-11 07:06:26 +0000

    Stock Updates pages UI

--- src/components/stockUpdates/StockUpdates.jsx
+++ src/components/stockUpdates/StockUpdates.jsx
@@ -12,72 +12,23 @@ const StockUpdates = () => {
                 <div class="card-body">
                   <h4 class="card-title">Stock Updates</h4>
               <p class="card-description">
-                    See full list by clicking <code>Action column</code>
+                    See full list of single products by clicking <code>Action column</code>
                   </p>
               <div class="table-responsive pt-3">
                     <table class="table table-bordered">
                       <thead>
                         <tr>
                           <th>#</th>
-                          <th>Product Categories</th>
+                          <th>Product Category</th>
                           <th>Total Products</th>
-                          <th>Amount</th>
-                          <th>Deadline</th>
+                          <th>Action</th>
                         </tr>
                       </thead>
                       <tbody>
                         <tr>
                           <td>1</td>
                           <td>Herman Beck</td>
-                          <td>
-                            <div class="progress">
-                              <div
-                                class="progress-bar bg-success"
-                                role="progressbar"
-                                style={{ width: "25%" }}
-                                aria-valuenow="25"
-                                aria-valuemin="0"
-                                aria-valuemax="100"
-                              ></div>
-                            </div>
-                          </td>
                           <td>$ 77.99</td>
-                          <td>May 15, 2015</td>
-                        </tr>
-                        <tr>
-                          <td>2</td>
-                          <td>Messsy Adam</td>
-                          <td>
-                            <div class="progress">
-                              <div
-                                class="progress-bar bg-danger"
-                                role="progressbar"
-                                style={{ width: "75%" }}
-                                aria-valuenow="75"
-                                aria-valuemin="0"
-                                aria-valuemax="100"
-                              ></div>
-                            </div>
-                          </td>
-                          <td>$245.30</td>
-                          <td>July 1, 2015</td>
-                        </tr>
-                        <tr>
-                          <td>3</td>
-                          <td>John Richards</td>
-                          <td>
-                            <div class="progress">
-                              <div
-                                class="progress-bar bg-warning"
-                                role="progressbar"
-                                aria-valuenow="90"
-                                aria-valuemin="0"
-                                aria-valuemax="100"
-                              ></div>
-                            </div>
-                          </td>
-                          <td>$138.00</td>
-                          <td>Apr 12, 2015</td>
                         </tr>
                       </tbody>
                     </table>
